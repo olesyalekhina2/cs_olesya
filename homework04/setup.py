892ed4e147dee01cf71d6f243333fbef0bee0b06
olesyalekhina2 <83693031+olesyalekhina2@users.noreply.github.com>
2021-05-05 10:45:33 +0300

    Update setup.py

--- homework04/setup.py
+++ homework04/setup.py
@@ -9,7 +9,7 @@ HOME_PAGE = "https://github.com/olesyalekhina2/cs_olesya"
 setup(
     name="pyvcs",
     version=pyvcs.__version__,
-    description="The stupid content tracker",
+    description="The content tracker",
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     packages=["pyvcs"],
